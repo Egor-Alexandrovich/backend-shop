a8f38cd7b61f3308c8e88c072a43605e792c0f1d
Yahor Shulha <yahor_shulha@epam.com>
2023-03-30 15:12:20 +0300

    Add cors true

--- product-service/handlers/getProductsById.ts
+++ product-service/handlers/getProductsById.ts
@@ -1,18 +1,56 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+type Product = {
+  id: string | undefined;
+  title: string;
+  description: string;
+  price: number;
+}
 
 export const getProductsById = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  console.log('event', event)
+  const products: Product[] = [
+    {
+      description: "Short Product Description1",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+      price: 24,
+      title: "ProductOne",
+    },
+    {
+      description: "Short Product Description7",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+      price: 15,
+      title: "ProductTitle",
+    },
+    {
+      description: "Short Product Description2",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+      price: 23,
+      title: "Product",
+    },
+    {
+      description: "Short Product Description4",
+      id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+      price: 15,
+      title: "ProductTest",
+    },
+    {
+      description: "Short Product Descriptio1",
+      id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+      price: 23,
+      title: "Product2",
+    },
+    {
+      description: "Short Product Description7",
+      id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+      price: 15,
+      title: "ProductName",
+    },
+  ];
+  const id = event?.pathParameters?.productId
+  const resp = products.filter((product: Product) => product.id === id);
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! getProductsById!",
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(resp),
   };
 };
--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -1,11 +1,54 @@
-// import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+type Product = {
+  id: string | undefined;
+  title: string;
+  description: string;
+  price: number;
+}
 
-export const getProductsList = async (event) => {
+export const getProductsList = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  const products: Product[] = [
+    {
+      description: "Short Product Description1",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
+      price: 24,
+      title: "ProductOne",
+    },
+    {
+      description: "Short Product Description7",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a1",
+      price: 15,
+      title: "ProductTitle",
+    },
+    {
+      description: "Short Product Description2",
+      id: "7567ec4b-b10c-48c5-9345-fc73c48a80a3",
+      price: 23,
+      title: "Product",
+    },
+    {
+      description: "Short Product Description4",
+      id: "7567ec4b-b10c-48c5-9345-fc73348a80a1",
+      price: 15,
+      title: "ProductTest",
+    },
+    {
+      description: "Short Product Descriptio1",
+      id: "7567ec4b-b10c-48c5-9445-fc73c48a80a2",
+      price: 23,
+      title: "Product2",
+    },
+    {
+      description: "Short Product Description7",
+      id: "7567ec4b-b10c-45c5-9345-fc73c48a80a1",
+      price: 15,
+      title: "ProductName",
+    },
+  ];
   return {
     statusCode: 200,
-    body: {
-      product: 'book',
-      price: 123
-    },
-  }
+    body: JSON.stringify(products),
+  };
 };
